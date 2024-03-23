9133dd5552de8c44b958bdf1fdcc4fb22921e493
Xapsiel <ilyas_useinov3301@mail.ru>
2024-03-23 21:28:27 +0300

    added a .env file

--- KEY/KEY.go
+++
@@ -1,3 +0,0 @@
-package KEY
-
-const KEY = "mongodb+srv://ilyasuseinov3301:mishka_2023@recipebook.xxu8dre.mongodb.net/?retryWrites=true&w=majority&appName=RecipeBook"
--- config/db.go
+++ config/db.go
@@ -1,19 +1,26 @@
 package config
 
 import (
-	"back-end/KEY"
 	"context"
+	"fmt"
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"os"
 )
 
 var MongoClient *mongo.Client
 
 func init() {
-	ConnectToDB(KEY.KEY)
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+	KEY := os.Getenv("KEY")
+	fmt.Println(KEY)
+	ConnectToDB(KEY)
 }
 func ConnectToDB(KEY string) {
-
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
 	opts := options.Client().ApplyURI(KEY).SetServerAPIOptions(serverAPI)
 
