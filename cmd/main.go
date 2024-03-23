cb565bc0fceb6e24fc3d250a1965470374d624dc
Xapsiel <ilyas_useinov3301@mail.ru>
2024-03-23 21:44:04 +0300

    new endpoint

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,6 @@ import (
 
 func main() {
 	r := gin.Default()
-	r.GET("/getRecipe/:page", (&controllers.RecipeController{}).GetRecipes)
+	r.GET("Mtaste/API/getRecipe/:page", (&controllers.RecipeController{}).GetRecipes)
 	r.Run()
 }
