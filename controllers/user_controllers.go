a3abd406b546d2031d58d2da04c7c400ed4eb520
Xapsiel <ilyas_useinov3301@mail.ru>
2024-06-05 19:32:16 +0300

    added a add/delete to/from favourite

--- controllers/user_controllers.go
+++ controllers/user_controllers.go
@@ -15,11 +15,11 @@ func (_ *UserController) AddToFavourite(c *gin.Context) {
 	var user_interface interface{}
 	c.Bind(&user_interface)
 	new := user_interface.(map[string]interface{})
-
 	var user = model.User{
-		UserId:   new["userID"].(int),
-		RecipeID: new["recipeID"].(int),
+		UserId:   int(new["userId"].(float64)),
+		RecipeID: int(new["recipeId"].(float64)),
 	}
+
 	result, flag, err, code := user.AddToFavourite()
 	if err != nil {
 		c.JSON(http.StatusOK, map[string]interface{}{
@@ -42,14 +42,15 @@ func (_ *UserController) AddToFavourite(c *gin.Context) {
 }
 
 func (_ *UserController) DeleteFromFavourite(c *gin.Context) {
+
 	var user_interface interface{}
 	c.Bind(&user_interface)
 	new := user_interface.(map[string]interface{})
-
 	var user = model.User{
-		UserId:   new["userID"].(int),
-		RecipeID: new["recipeID"].(int),
+		UserId:   int(new["userId"].(float64)),
+		RecipeID: int(new["recipeId"].(float64)),
 	}
+
 	result, flag, err, code := user.DeleteFromFavourite()
 	if err != nil {
 		c.JSON(http.StatusOK, map[string]interface{}{
