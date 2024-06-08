721fa7cf0d3a2a0ddf025d024a431f3000ab449c
Xapsiel <ilyas_useinov3301@mail.ru>
2024-06-08 16:41:14 +0300

    AA

--- config/db.go
+++ config/db.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
 
@@ -23,9 +24,11 @@ func init() {
 	}
 }
 func ConnectToDB() error {
+
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-	opts := options.Client().ApplyURI("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.6").SetServerAPIOptions(serverAPI)
-	client, err := mongo.Connect(context.Background(), opts)
+	opts := options.Client().ApplyURI(fmt.Sprintf("mongodb+srv://ilyasuseinov3301:abc2024@recipebook.xxu8dre.mongodb.net/")).SetServerAPIOptions(serverAPI)
+	client, err := mongo.Connect(context.TODO(), opts)
+	fmt.Println(err, "1")
 	if err != nil {
 		return err
 	}
