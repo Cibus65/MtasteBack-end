98452a7806128990f326d1d43ea4abe4d80789da
Xapsiel <ilyas_useinov3301@mail.ru>
2024-06-08 15:47:01 +0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- config/db.go
+++ config/db.go
@@ -22,7 +22,7 @@ func init() {
 func ConnectToDB() error {
 
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-	opts := options.Client().ApplyURI(fmt.Sprintf("mongodb+srv://ilyasuseinov3301:abc2024@recipebook.xxu8dre.mongodb.net/")).SetServerAPIOptions(serverAPI)
+	opts := options.Client().ApplyURI(fmt.Sprintf("mongodb+srv://ilyasuseinov3301:abc2024@recipebook.xxu8dre.mongodb.net/?retryWrites=true&w=majority&appName=RecipeBook")).SetServerAPIOptions(serverAPI)
 	client, err := mongo.Connect(context.TODO(), opts)
 	fmt.Println(err, "1")
 	if err != nil {
