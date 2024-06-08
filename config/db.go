6f42746b228eebbcd88286d588e4e26c23ec6fea
Xapsiel <ilyas_useinov3301@mail.ru>
2024-06-08 16:36:32 +0300

    AA

--- config/db.go
+++ config/db.go
@@ -24,7 +24,7 @@ func init() {
 }
 func ConnectToDB() error {
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-	opts := options.Client().ApplyURI("mongodb://localhost:27017").SetServerAPIOptions(serverAPI)
+	opts := options.Client().ApplyURI("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.6").SetServerAPIOptions(serverAPI)
 	client, err := mongo.Connect(context.Background(), opts)
 	if err != nil {
 		return err
