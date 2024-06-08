d47cc420db15390d7b7d819cb946bd893a9ed73b
Xapsiel <ilyas_useinov3301@mail.ru>
2024-06-08 15:52:28 +0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- config/db.go
+++ config/db.go
@@ -20,14 +20,22 @@ func init() {
 	}
 }
 func ConnectToDB() error {
-
+	// Устанавливаем параметры API сервера MongoDB
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-	opts := options.Client().ApplyURI(fmt.Sprintf("mongodb+srv://ilyasuseinov3301:abc2024@recipebook.xxu8dre.mongodb.net/?retryWrites=true&w=majority&appName=RecipeBook")).SetServerAPIOptions(serverAPI)
+
+	// Создаем параметры подключения к MongoDB
+	opts := options.Client().ApplyURI(fmt.Sprintf("mongodb+srv://ilyasuseinov3301:abc2024@recipebook.xxu8dre.mongodb.net/?retryWrites=true&w=majority&appName=RecipeBook"))
+	// Добавляем параметры API сервера к параметрам подключения
+	opts.SetServerAPIOptions(serverAPI)
+
+	// Подключаемся к MongoDB
 	client, err := mongo.Connect(context.TODO(), opts)
 	fmt.Println(err, "1")
 	if err != nil {
 		return err
 	}
+
+	// Сохраняем клиент для дальнейшего использования
 	MongoClient = client
 	return nil
 }
