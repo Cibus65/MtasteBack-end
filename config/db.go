4af0382b3e8b25903f611189ff2046b2aa983331
Xapsiel <ilyas_useinov3301@mail.ru>
2024-06-08 16:00:37 +0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- config/db.go
+++ config/db.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -12,30 +13,25 @@ import (
 var MongoClient *mongo.Client
 
 func init() {
-
-	err := ConnectToDB()
-	fmt.Println(err, "0")
+	file, err := os.OpenFile("../app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	log.SetOutput(file)
+	if err != nil {
+		panic(err)
+	}
+	err = ConnectToDB()
 	if err != nil {
 		log.Fatalf("Failed connection to MongoDB:\n\t", err)
 	}
 }
 func ConnectToDB() error {
-	// Устанавливаем параметры API сервера MongoDB
+
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-
-	// Создаем параметры подключения к MongoDB
-	opts := options.Client().ApplyURI(fmt.Sprintf("mongodb+srv://ilyasuseinov3301:abc2024@recipebook.xxu8dre.mongodb.net/?retryWrites=true&w=majority&appName=RecipeBook"))
-	// Добавляем параметры API сервера к параметрам подключения
-	opts.SetServerAPIOptions(serverAPI)
-
-	// Подключаемся к MongoDB
+	opts := options.Client().ApplyURI(fmt.Sprintf("mongodb://localhost:27017/")).SetServerAPIOptions(serverAPI)
 	client, err := mongo.Connect(context.TODO(), opts)
 	fmt.Println(err, "1")
 	if err != nil {
 		return err
 	}
-
-	// Сохраняем клиент для дальнейшего использования
 	MongoClient = client
 	return nil
 }
